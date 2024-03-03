901ab6ad6eddc132fc81a035c78a79a9e79a29dd
Felo <felogmousa@gmail.com>
2024-03-02 22:31:13 -0800

    Send Playere Health data to webserver in JSON format

--- src/main/java/com/felomousa/rad/client/radclient.java
+++ src/main/java/com/felomousa/rad/client/radclient.java
@@ -4,26 +4,56 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class radclient implements ClientModInitializer {
     private static final Logger LOGGER = LogManager.getLogger(radclient.class);
 
-    private double lastHealth = -1.0; // Initialize to a value that's unlikely to be a real health value
+    private double lastHealth = -1.0;
+    private static final String PI_SERVER_URL = "http://your_pi_ip_address:your_port/update_health";
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info("RAD mod initializing..."); // Verify initialization
+        LOGGER.info("RAD mod initializing...");
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player != null) {
                 double currentHealth = client.player.getHealth();
                 if (currentHealth != lastHealth) {
-                    LOGGER.info("Player Health Changed: " + currentHealth); // Use LOGGER here
+                    LOGGER.info("Player Health Changed: " + currentHealth);
+                    sendHealthData(currentHealth);
                     lastHealth = currentHealth;
                 }
             } else {
-                LOGGER.debug("Client player is null"); // This line is for debugging purposes
+                LOGGER.debug("Client player is null");
             }
         });
+    }
+
+    private void sendHealthData(double health) {
+        try {
+            URL url = new URL(PI_SERVER_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+
+            String jsonPayload = "{\"health\": " + health + "}";
+
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonPayload.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            int responseCode = conn.getResponseCode();
+            LOGGER.info("HTTP Response Code: " + responseCode);
+
+            conn.disconnect();
+        } catch (Exception e) {
+            LOGGER.error("Error sending health data: " + e.getMessage());
+        }
     }
 }
